12c9cb38a21fc5fbe18cce586e9d046389438c82
Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
2011-03-02 00:52:04 -0500

    Eliminado index.php

--- web/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__.'/../app/AlboradaKernel.php';
-
-use Symfony\Component\HttpFoundation\Request;
-
-$kernel = new AlboradaKernel('prod', false);
-$kernel->handle(new Request())->send();
